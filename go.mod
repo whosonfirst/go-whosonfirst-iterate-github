30ae8c9c0535d28e48ccef368b3b1026c6688ac9
thisisaaronland <thisisaaronland@localhost>
2025-04-22 12:03:08 -0700

    update vendor deps

--- go.mod
+++ go.mod
@@ -1,11 +1,25 @@
 module github.com/whosonfirst/go-whosonfirst-iterate-github/v2
 
-go 1.16
+go 1.23.0
+
+toolchain go1.24.2
 
 require (
 	github.com/google/go-github v17.0.0+incompatible
+	github.com/whosonfirst/go-ioutil v1.0.2
+	github.com/whosonfirst/go-whosonfirst-iterate/v2 v2.6.0
+	golang.org/x/oauth2 v0.29.0
+)
+
+require (
+	github.com/aaronland/go-json-query v0.1.6 // indirect
+	github.com/aaronland/go-roster v1.0.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
-	github.com/whosonfirst/go-ioutil v1.0.0
-	github.com/whosonfirst/go-whosonfirst-iterate/v2 v2.0.0
-	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f
+	github.com/tidwall/gjson v1.18.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/whosonfirst/go-whosonfirst-crawl v0.2.2 // indirect
+	github.com/whosonfirst/go-whosonfirst-sources v0.1.0 // indirect
+	github.com/whosonfirst/go-whosonfirst-uri v1.3.0 // indirect
+	github.com/whosonfirst/walk v0.0.2 // indirect
 )
--- vendor/github.com/whosonfirst/go-whosonfirst-iterate/v2/filters/query.go
+++ vendor/github.com/whosonfirst/go-whosonfirst-iterate/v2/filters/query.go
@@ -2,35 +2,82 @@ package filters
 
 import (
 	"context"
-	"github.com/aaronland/go-json-query"
+	"fmt"
 	"io"
 	"net/url"
+
+	"github.com/aaronland/go-json-query"
 )
 
+// type QueryFilters implements the `Filters` interface for filtering documents using `aaronland/go-json-query` query strings.
 type QueryFilters struct {
 	Filters
+	// Include is a `aaronland/go-json-query.QuerySet` instance containing rules that must match for a document to be considered for further processing.
 	Include *query.QuerySet
+	// Exclude is a `aaronland/go-json-query.QuerySet` instance containing rules that if matched will prevent a document from being considered for further processing.
 	Exclude *query.QuerySet
 }
 
+// NewQueryFiltersFromURI() will return a `QueryFilters` instance derived from 'uri' which is expected
+// to take the form of:
+//
+//	{SCHEME}://{HOST}?{PARAMETERS}
+//
+// Where {SCHEME} and {HOST} can be any value so long as they conform to the URI standard. {PARAMETERS} is expected to be
+// zero or more of the following parameters:
+// * `?include=` Zero or more `aaronland/go-json-query` query strings containing rules that must match for a document to be considered for further processing.
+// * `?exclude=` Zero or more `aaronland/go-json-query`	query strings containing rules that if matched will prevent a document from being considered for further processing.
+// * `?include_mode=` A valid `aaronland/go-json-query` query mode string for testing inclusion rules.
+// * `?exclude_mode=` A valid `aaronland/go-json-query` query mode string for testing exclusion rules.
 func NewQueryFiltersFromURI(ctx context.Context, uri string) (Filters, error) {
 
 	u, err := url.Parse(uri)
 
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Failed to parse URI, %w", err)
 	}
 
 	q := u.Query()
 	return NewQueryFiltersFromQuery(ctx, q)
 }
 
+// NewQueryFiltersFromQuery() will return a `QueryFilters` instance derived from 'q' which is expected
+// to contain zero or more of the following parameters:
+// * `?include=` Zero or more `aaronland/go-json-query` query strings containing rules that must match for a document to be considered for further processing.
+// * `?exclude=` Zero or more `aaronland/go-json-query`	query strings containing rules that if matched will prevent a document from being considered for further processing.
+// * `?include_mode=` A valid `aaronland/go-json-query` query mode string for testing inclusion rules.
+// * `?exclude_mode=` A valid `aaronland/go-json-query` query mode string for testing exclusion rules.
 func NewQueryFiltersFromQuery(ctx context.Context, q url.Values) (Filters, error) {
 
 	f := &QueryFilters{}
 
-	includes := q["include"]
-	excludes := q["exclude"]
+	q_includes := q["include"]
+	q_excludes := q["exclude"]
+
+	includes := make([]string, len(q_includes))
+	excludes := make([]string, len(q_excludes))
+
+	for idx, q_enc := range q_includes {
+
+		q, err := url.QueryUnescape(q_enc)
+
+		if err != nil {
+			return nil, fmt.Errorf("Failed to unescape '%s' include parameter, %w", q_enc, err)
+		}
+
+		includes[idx] = q
+	}
+
+	for idx, q_enc := range q_excludes {
+
+		q, err := url.QueryUnescape(q_enc)
+
+		if err != nil {
+			return nil, fmt.Errorf("Failed to unescape '%s' exclude parameter, %w", q_enc, err)
+		}
+
+		excludes[idx] = q
+	}
 
 	if len(includes) > 0 {
 
@@ -38,7 +85,7 @@ func NewQueryFiltersFromQuery(ctx context.Context, q url.Values) (Filters, error
 		includes_qs, err := querySetFromStrings(ctx, includes_mode, includes...)
 
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("Failed to construct include query set, %w", err)
 		}
 
 		f.Include = includes_qs
@@ -50,7 +97,7 @@ func NewQueryFiltersFromQuery(ctx context.Context, q url.Values) (Filters, error
 		excludes_qs, err := querySetFromStrings(ctx, excludes_mode, excludes...)
 
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("Failed to construct exclude query set, %w", err)
 		}
 
 		f.Exclude = excludes_qs
@@ -59,6 +106,7 @@ func NewQueryFiltersFromQuery(ctx context.Context, q url.Values) (Filters, error
 	return f, nil
 }
 
+// querySetFromStrings() will return a `query.QuerySet` instance derive from 'mode' and 'flags'.
 func querySetFromStrings(ctx context.Context, mode string, flags ...string) (*query.QuerySet, error) {
 
 	var query_flags query.QueryFlags
@@ -69,7 +117,7 @@ func querySetFromStrings(ctx context.Context, mode string, flags ...string) (*qu
 		err := query_flags.Set(fl)
 
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("Failed to assign '%s' to query flags, %w", fl, err)
 		}
 	}
 
@@ -88,12 +136,13 @@ func querySetFromStrings(ctx context.Context, mode string, flags ...string) (*qu
 	return qs, nil
 }
 
-func (f *QueryFilters) Apply(ctx context.Context, fh io.ReadSeekCloser) (bool, error) {
+// Apply() performs filtering operations against 'fh' and returns a boolean value indicating whether the record should be considered for further processing.
+func (f *QueryFilters) Apply(ctx context.Context, fh io.ReadSeeker) (bool, error) {
 
 	body, err := io.ReadAll(fh)
 
 	if err != nil {
-		return false, err
+		return false, fmt.Errorf("Failed to read document, %w", err)
 	}
 
 	includes_qs := f.Include
@@ -104,7 +153,7 @@ func (f *QueryFilters) Apply(ctx context.Context, fh io.ReadSeekCloser) (bool, e
 		matches, err := query.Matches(ctx, includes_qs, body)
 
 		if err != nil {
-			return false, err
+			return false, fmt.Errorf("Failed to perform includes matching, %w", err)
 		}
 
 		if !matches {
@@ -118,7 +167,7 @@ func (f *QueryFilters) Apply(ctx context.Context, fh io.ReadSeekCloser) (bool, e
 		matches, err := query.Matches(ctx, excludes_qs, body)
 
 		if err != nil {
-			return false, err
+			return false, fmt.Errorf("Failed to perform excludes matching, %w", err)
 		}
 
 		if matches {
